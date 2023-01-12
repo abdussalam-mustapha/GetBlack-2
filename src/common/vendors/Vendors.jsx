1574e733f40d756b8539aa3416862cc0d0b137e6
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2023-01-12 05:14:44 +0100

    new update

--- src/common/vendors/Vendors.jsx
+++ src/common/vendors/Vendors.jsx
@@ -9,19 +9,19 @@ const vendors = () => {
     style={{ background: `url(${imageUrl}) center center / cover no-repeat` }}
     className="vendors-img"
   >
-    <div className="container mx-auto p-4">
-      <div className="flex justify-start items-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 mt-10 xl:mt-40">
-          <h1 className="text-2xl lg:text-4xl xl:text-5xl font-extrabold text-black">
+    <div className=" v_flex">
+      <div className="vp_flex">
+        <div className="">
+          <h1 className="">
             Reach new customers and get more orders with GetBlack.
           </h1>
         </div>
       </div>
-      <div className="grid grid-cols-2">
-        <div className="mt-4 w-[200px]">
+      <div className="vp_btn">
+        <div className="vp_btn-main">
           <Link
             to="/vendor-register"
-            className="btn bg-[#FFD700] hover:bg-[#52CD8B] border-none text-black p-2 md:p-3 font-bold uppercase text-md rounded-lg w-full"
+            className="v_btn bg-[#FFD700] hover:bg-[#52CD8B] border-none text-black p-2 md:p-3 font-bold uppercase text-md rounded-lg w-full"
           >
             Apply Now
           </Link>
